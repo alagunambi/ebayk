9ace203c005bd8ae26a064cdf3b25c6458db9b7e
Prem <prem89k@gmail.com>
2015-09-13 04:27:22 +0530

    Add export

--- app/assets/javascripts/ads.js
+++ app/assets/javascripts/ads.js
@@ -1,5 +1,21 @@
+
+var ready;
+
+ready = function(){
+
 $('#start-button').on('click', function () {
   var $btn = $(this).button('loading')
   // business logic...
   $btn.button('reset')
 })
+
+
+ $(document).ready(function(){
+    $('#export-details_from_date').datepicker({"autoclose": true});
+    $('#export-details_to_date').datepicker({"autoclose": true});
+  });
+
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :ads
   match "/scrapping" => "visitors#scrapping", via: [ :post, :get ]
   match "/destroy_all" => "visitors#destroy_all", via: [ :post, :get ]
+  post 'ads/today_csv' => "ads#today_csv"
+  post '/export_with_date' => 'ads#export_with_date'
   root to: 'visitors#index'
+
+  
 end
